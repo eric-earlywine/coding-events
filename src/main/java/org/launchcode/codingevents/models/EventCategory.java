d439a47760669dd340d0a4976ade5c2042150960
Eric <eric.earlywine@hotmail.com>
2020-01-26 16:48:55 -0600

    Added One to Many and Many to One relationships between Event and Event Category

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -1,11 +1,21 @@
 package org.launchcode.codingevents.models;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class EventCategory extends AbstractEntity {
+    @OneToMany(mappedBy = "eventCategory")
+    private final List<Event> events = new ArrayList<>();
+
     public EventCategory(String name) {
         super(name);
     }
     public EventCategory() {}
+
+    public List<Event> getEvents() {
+        return events;
+    }
 }
